1a6419d2452dc6082b2dcf11cae5614da4340816
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-05-07 04:06:10 +0000

    Updated a lot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
     if the_post.valid?
       the_post.save
-      redirect_to("/posts", { :notice => "Post created successfully." })
+      redirect_to("/boards/#{the_post.board_id}", { :notice => "Post created successfully." })
     else
       redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
     end
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     the_post.board_id = params.fetch("query_board_id")
 
     if the_post.valid?
-      the_post.save
+       the_post.save
       redirect_to("/posts/#{the_post.id}", { :notice => "Post updated successfully."} )
     else
       redirect_to("/posts/#{the_post.id}", { :alert => the_post.errors.full_messages.to_sentence })
